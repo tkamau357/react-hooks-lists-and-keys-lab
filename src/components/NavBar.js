fc18bae7a9c2bb63a15b0e0499c3cf0992207862
johnthairukamau <tkamau82@gmail.com>
2023-05-01 11:10:12 +0300

    Completed lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,15 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    <ul>
+        {links.map(link => (
+          <li key={link}>
+            <a href={`/${link.toLowerCase()}`}>{link}</a>
+          </li>
+        ))}
+      </ul>
+  </nav>;
 }
 
 export default NavBar;
